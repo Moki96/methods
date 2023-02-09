66a4b31c58e70ce0f2d404b087fca252a7529f7d
КрахМастер КрахМастер <mokrushin.sr@gmail.com>
2023-02-09 15:31:45 +0500

    Добавьте файлы проекта.

---
+++ methods/Program.cs
@@ -0,0 +1,31 @@
+﻿static void Print(string TextPrint)
+{
+    Console.WriteLine(TextPrint);
+}
+
+
+static string[] SplitText(string text)
+
+{
+
+
+    string[] SplitString = text.Split(new char[] { ' ' });
+
+    OutputText(SplitString);
+    return SplitString;
+
+}
+
+
+static void OutputText(string[] Args)
+{
+    Console.WriteLine(Args);
+    for (int i = 0; i < Args.Length; i++)
+    {
+        Console.WriteLine(Args[i]);
+    }
+}
+
+
+Print("Введите свое длинное предложение");
+SplitText(Console.ReadLine());
---
+++ methods2/Program.cs
@@ -0,0 +1,22 @@
+﻿static string SplitWord2(string text)
+{
+    string[] WordsSplit2 = text.Split(' ');
+    string newText = ReverseWord(WordsSplit2);
+    return newText;
+}
+static string ReverseWord(string[] text)
+{
+    Array.Reverse(text);
+    string str = string.Join(" ", text);
+    return str;
+}
+
+Console.WriteLine("Введите предложение: ");
+
+string invertedText = Console.ReadLine();
+
+invertedText = SplitWord2(invertedText);
+
+
+Console.WriteLine(invertedText);
+Console.ReadLine();
